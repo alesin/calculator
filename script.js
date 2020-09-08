bcb6dc95657225dfdacd88369754e407eb0e0a2b
Anna Breslaw <annabreslaw@gmail.com>
2020-09-08 13:04:13 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let result
+
+function add(num1, num2){
+    result = (num1 + num2)
+    return result;
+}
+
+function subtract(num1, num2){
+    result = (num1 + num2)
+    return result;
+}
+
+function multiply(num1, num2){
+    result = (num1 * num2)
+    return result;
+}
+
+function divide(num1, num2){
+    result = (num1 * num2)
+    return result;
+}
+
+function operate(operator, num1, num2){
+    
+}
