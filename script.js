1743110243031bcc83fc248998ca98a37d59e8ba
alesin <alesintipler@gmail.com>
2020-09-09 02:43:23 -0400

    feat: manage user input (numbers) and display user input

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
-let result
 
+let num1 = null
+let num2 = null
+let operator = ''
+let result = null
+
+// *** DOM connection ***
+const calcInput = document.getElementById('calc-input')
+const calcOutput = document.getElementById('calc-output')
+
+calcInput.addEventListener('click', (event) => {
+    // console.log(event.target.textContent)
+    // *** TD = operators
+    if (event.target.tagName === 'TD') {
+        let userInput = event.target.innerText
+        console.log(userInput)
+
+        if (userInput === '=') {
+            // operate()
+        }
+        else {
+            operator = userInput
+        }
+    }
+    // *** TH = numbers
+    if (event.target.tagName === 'TH') {
+        let userInput = event.target.innerText
+        console.log(userInput)
+
+        // *** if user has not selected OPERATOR, adjust num1
+        if (operator === '') {
+            if (!num1) {
+                num1 = userInput
+            }
+            else {
+                num1 += userInput
+            }
+        }
+        else {
+            if (!num2) {
+                num2 = userInput
+            }
+            else {
+                num2 += userInput
+            }
+        }
+    }
+
+    console.log("Number #1", num1)
+    calcOutput.textContent = calcOutput.textContent + event.target.innerText
+})
+
+// *** BASIC FUNCTIONS ***
 function add(num1, num2){
-    result = (num1 + num2)
+    result = (Number(num1) + Number(num2))
     return result;
 }
 
@@ -21,10 +72,18 @@ function divide(num1, num2){
 }
 
 function operate(operator, num1, num2){
-    // if (operator = 'add')
+    switch (operator) {
+        case '+':
+            return add(num1, num2)
+        case '-':
+            return subtract(num1, num2)
+        case 'x':
+            return multiply(num1, num2)
+        case '/':
+            return divide(num1, num2)
+        default:
+            return 'error'
+    }
 }
 
-// console.log(add(1,2))
-// console.log(subtract(1,2))
-// console.log(multiply(2,10))
-// console.log(divide(2,10))
+// console.log(operate('-', 8, 2))
